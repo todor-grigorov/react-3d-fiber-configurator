f4cffd0aec2fa4d87b241015b4e45e3ec194b2f0
todor-grigorov <t.grigorov99@gmail.com>
2023-04-23 19:42:13 +0200

    feat: add valtio state to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { useSnapshot } from 'valtio'
 
-
+import state from '../store'
 
 // import {
 //   headContainerAnimation,
@@ -12,6 +12,8 @@ import { useSnapshot } from 'valtio'
 // } from '../config/motion'
 
 const Home: React.FC = (): JSX.Element => {
+  const snap = useSnapshot(state)
+  
   return <div>Home</div>
 }
 
