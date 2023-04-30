0064e93de3f13596c52b90b58a05c3eb4d71ceb9
todor-grigorov <t.grigorov99@gmail.com>
2023-04-30 13:11:28 +0200

    feat: add more UI elements to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,6 +11,7 @@ import {
   headTextAnimation,
   slideAnimation
 } from '../config/motion'
+import { CustomButton } from '../components'
 
 const Home: React.FC = (): JSX.Element => {
   const snap = useSnapshot(state)
@@ -19,9 +20,27 @@ const Home: React.FC = (): JSX.Element => {
     <AnimatePresence>
       {snap.intro && (
         <motion.div className="home" {...slideAnimation(Direction.LEFT)}>
-            <motion.header>
+            <motion.header {...slideAnimation(Direction.DOWN)}>
               <img src="./threejs.png" alt="logo" className="w-8 h-8 object-contain" />
             </motion.header>
+
+            <motion.div className="home-content" {...headContainerAnimation}>
+              <motion.div {...headTextAnimation}>
+                <h1 className="head-text">
+                  LET'S <br className="xl:block hidden" /> DOI IT.
+                </h1>
+              </motion.div>
+
+              <motion.div {...headContentAnimation} className="flex flex-col gap-5">
+                <p className="max-w-md font-normal text-grey-600 text-base">
+                  Create your unique and wxclusive shirt with our brand-new 3D customization tool. 
+                  <strong>Unleash your imagination</strong>{" "}
+                  and define your own style.
+                </p>
+
+                <CustomButton type="filled" title="Customize It" customStyles="w-fit px-4 py-2.5 font-bold text-sm" handleClick={() => state.intro = false} />
+              </motion.div>
+            </motion.div>
         </motion.div>
       )}
     </AnimatePresence>
