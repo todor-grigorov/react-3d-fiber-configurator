4ff9f411839fe50931d6a81b056e95758e7acfe8
todor-grigorov <t.grigorov99@gmail.com>
2023-04-30 14:47:43 +0200

    feat: add imports

--- src/pages/Customizer.tsx
+++ src/pages/Customizer.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
+import { useSnapshot } from 'valtio'
+
+import config from '../config/config'
+import state from '../store'
+import { download } from '../assets'
+import { downloadCanvasToImage, reader } from '../config/helpers'
+import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants'
+import { fadeAnimation, slideAnimation } from '../config/motion'
+
 
 const Customizer: React.FC = (): JSX.Element => {
+  
   return <div>Customizer</div>
 }
 
