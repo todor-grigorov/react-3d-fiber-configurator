4a4094809de4bdd36726de2ffe1cbde464c0c1d9
todor-grigorov <t.grigorov99@gmail.com>
2023-04-30 17:00:34 +0200

    feat: add more UI elements

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
-const Tab: React.FC = (): JSX.Element => {
+interface ParentProps {
+    tab: TabType
+    handleClick: (e: any) => void
+}
+
+type Props = ParentProps
+
+const Tab: React.FC<Props> = ({tab, handleClick}): JSX.Element => {
   return (
     <div>Tab</div>
   )
