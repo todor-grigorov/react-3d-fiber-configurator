916203eaf574f9f232c2cad4398737eda8a39bc3
todor-grigorov <t.grigorov99@gmail.com>
2023-05-02 21:56:08 +0200

    feat: add imports

--- src/canvas/Shirt.tsx
+++ src/canvas/Shirt.tsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import { easing } from 'maath'
+import { useSnapshot } from 'valtio'
+import { useFrame } from '@react-three/fiber'
+import { Decal, useGLTF, useTexture } from '@react-three/drei'
+
+import state from '../store'
 
 const Shirt = () => {
   return (
