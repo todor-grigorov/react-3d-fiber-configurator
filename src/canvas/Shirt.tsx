9748c7a813231803d2a34f877c48bec35a4f1a20
todor-grigorov <t.grigorov99@gmail.com>
2023-05-02 22:10:24 +0200

    feat: generate shirt gltf model

--- src/canvas/Shirt.tsx
+++ src/canvas/Shirt.tsx
@@ -1,14 +1,31 @@
 import React from 'react'
+import * as THREE from 'three'
 import { easing } from 'maath'
 import { useSnapshot } from 'valtio'
 import { useFrame } from '@react-three/fiber'
 import { Decal, useGLTF, useTexture } from '@react-three/drei'
+import { GLTF } from 'three-stdlib'
 
 import state from '../store'
 
-const Shirt = () => {
+
+type GLTFResult = GLTF & {
+  nodes: {
+    T_Shirt_male: THREE.Mesh
+  }
+  materials: {
+    lambert1: THREE.MeshStandardMaterial
+  }
+}
+
+const Shirt = (props: JSX.IntrinsicElements['group']) => {
+const snap = useSnapshot(state)
+const {nodes, materials} = useGLTF('/shirt_baked.glb') as GLTFResult
+
   return (
-    <div>Shirt</div>
+    <group {...props} dispose={null}>
+      <mesh geometry={nodes.T_Shirt_male.geometry} material={materials.lambert1} />
+    </group>
   )
 }
 
