2d2cae622855a50bafd66db53aed54cf94f8b501
todor-grigorov <t.grigorov99@gmail.com>
2023-05-03 22:54:26 +0200

    fix: imports in CameraRig component

--- src/canvas/CameraRig.tsx
+++ src/canvas/CameraRig.tsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import { useFrame } from '@react-three/fiber'
+import { easing } from 'maath'
+import { useSnapshot } from 'valtio'
+
+import state from '../store'
 
 interface ParentProps {
 }
