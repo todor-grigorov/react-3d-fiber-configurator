f0882cc06762d0368d0c9586b30408463733752e
todor-grigorov <t.grigorov99@gmail.com>
2023-05-05 22:51:09 +0200

    fix: imports in backdrop component

--- src/canvas/Backdrop.tsx
+++ src/canvas/Backdrop.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { easing } from 'maath'
+import { useFrame } from '@react-three/fiber'
+import { AccumulativeShadows, RandomizedLight } from '@react-three/drei'
+ 
 
 const Backdrop = () => {
   return (
