2b2d91602a1cf58142a8550f87057d29692c19a1
todor-grigorov <t.grigorov99@gmail.com>
2023-05-06 21:21:48 +0200

    feat: add shadows

--- src/canvas/Backdrop.tsx
+++ src/canvas/Backdrop.tsx
@@ -6,7 +6,12 @@ import { AccumulativeShadows, RandomizedLight } from '@react-three/drei'
 
 const Backdrop = () => {
   return (
-    <></>
+    <AccumulativeShadows
+      position={[0, 0, -0.14 ]}
+    >
+      <RandomizedLight
+        amount={4} />
+    </AccumulativeShadows>
   )
 }
 
