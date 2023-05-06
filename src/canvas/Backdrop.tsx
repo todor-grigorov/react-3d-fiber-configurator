3dd4609c4c75205c9a8a286579daf1fd53dc39be
todor-grigorov <t.grigorov99@gmail.com>
2023-05-06 21:31:07 +0200

    feat: add first randomized light

--- src/canvas/Backdrop.tsx
+++ src/canvas/Backdrop.tsx
@@ -18,7 +18,12 @@ const Backdrop = () => {
       position={[0, 0, -0.14 ]}
     >
       <RandomizedLight
-        amount={4} />
+        amount={4} 
+        radius={9}
+        intensity={0.55}
+        ambient={0.25}
+        position={[5, 5, -10]}
+        />
     </AccumulativeShadows>
   )
 }
