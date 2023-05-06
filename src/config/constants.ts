b70270ec698641df5378028d1760b2e4d09ce62f
todor-grigorov <t.grigorov99@gmail.com>
2023-05-06 22:32:18 +0200

    fix: function FilterTabs in constants file

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,39 +1,41 @@
-import { TabType } from "../Types/TabType";
-import { swatch, fileIcon, ai, logoShirt, stylishShirt } from "../assets";
+import { EditorTabName } from '../Types/EditorTabName'
+import { FilterTabName } from '../Types/FilterTabName'
+import { TabType } from '../Types/TabType'
+import { swatch, fileIcon, ai, logoShirt, stylishShirt } from '../assets'
 
 export const EditorTabs: TabType[] = [
   {
-    name: "colorpicker",
+    name: EditorTabName.ColorPicker,
     icon: swatch,
   },
   {
-    name: "filepicker",
+    name: EditorTabName.FilePicker,
     icon: fileIcon,
   },
   {
-    name: "aipicker",
+    name: EditorTabName.AiPicker,
     icon: ai,
   },
-];
+]
 
 export const FilterTabs = [
   {
-    name: "logoShirt",
+    name: FilterTabName.LogoShirt,
     icon: logoShirt,
   },
   {
-    name: "stylishShirt",
+    name: FilterTabName.StylishShirt,
     icon: stylishShirt,
   },
-];
+]
 
 export const DecalTypes = {
   logo: {
-    stateProperty: "logoDecal",
-    filterTab: "logoShirt",
+    stateProperty: 'logoDecal',
+    filterTab: 'logoShirt',
   },
   full: {
-    stateProperty: "fullDecal",
-    filterTab: "stylishShirt",
+    stateProperty: 'fullDecal',
+    filterTab: 'stylishShirt',
   },
-};
+}
