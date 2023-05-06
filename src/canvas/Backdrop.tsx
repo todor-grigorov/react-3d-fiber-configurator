f375a491d19f974479d6fe8c5494fff9249c3186
todor-grigorov <t.grigorov99@gmail.com>
2023-05-06 21:28:47 +0200

    feat: adjust shadows

--- src/canvas/Backdrop.tsx
+++ src/canvas/Backdrop.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { easing } from 'maath'
 import { useFrame } from '@react-three/fiber'
 import { AccumulativeShadows, RandomizedLight } from '@react-three/drei'
  
 
 const Backdrop = () => {
+  const shadows = useRef(null)
+  
   return (
     <AccumulativeShadows
+      ref={shadows}
+      temporal
+      frames={60}
+      alphaTest={0.85}
+      scale={10}
+      rotation={[Math.PI / 2, 0, 0]}
       position={[0, 0, -0.14 ]}
     >
       <RandomizedLight
