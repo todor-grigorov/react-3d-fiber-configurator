f8f5fd767e099f1046f253642bec0a9de8ed3191
todor-grigorov <t.grigorov99@gmail.com>
2023-05-07 17:51:57 +0200

    feat: finish color picker

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { SketchPicker } from 'react-color'
+import { useSnapshot } from 'valtio'
+
+import state from '../store'
 
 const ColorPicker: React.FC = (): JSX.Element => {
+  const snap = useSnapshot(state)
+
   return (
-    <div>ColorPicker</div>
+    <div className="absolute left-full ml-3">
+      <SketchPicker
+        color={snap.color}
+        disableAlpha
+        // presetColors={[
+        //   '#000000',
+        //   '#ffffff',
+        //   '#ff0000',
+        //   '#00ff00',
+        //   '#0000ff',
+        //   '#ffff00',
+        //   '#00ffff',
+        //   '#ff00ff',
+        // ]}
+        onChange={(color) => (state.color = color.hex)}
+      />
+    </div>
   )
 }
 
