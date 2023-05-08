0ed4804c71e9f9e2def0cf938533856de5dcf8e6
todor-grigorov <t.grigorov99@gmail.com>
2023-05-08 21:30:59 +0200

    feat: set FilePIcker props

--- src/components/FilePicker.tsx
+++ src/components/FilePicker.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
+type ParentProps = {
+  file: string
+  setFile: React.Dispatch<React.SetStateAction<string>>
+}
+
+type Props = ParentProps
+
 const FilePicker: React.FC = (): JSX.Element => {
-  return (
-    <div>FilePicker</div>
-  )
+  return <div>FilePicker</div>
 }
 
 export default FilePicker
