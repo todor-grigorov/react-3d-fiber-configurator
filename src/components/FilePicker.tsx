299645c62153ffb631674ac21e6a3efee45ec02d
todor-grigorov <t.grigorov99@gmail.com>
2023-05-13 23:47:46 +0200

    feat: add upload button

--- src/components/FilePicker.tsx
+++ src/components/FilePicker.tsx
@@ -10,8 +10,34 @@ type ParentProps = {
 
 type Props = ParentProps
 
-const FilePicker: React.FC<Props> = ({ file, setFile }: Props): JSX.Element => {
-  return <div>FilePicker</div>
+const FilePicker: React.FC<Props> = ({
+  file,
+  setFile,
+  readFile,
+}: Props): JSX.Element => {
+  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files === null) {
+      setFile('')
+      return
+    }
+    setFile(e.target.files[0].name)
+  }
+
+  return (
+    <div className="filepicker-container">
+      <div className="flex-1 flex flex-col">
+        <input
+          id="file-upload"
+          type="file"
+          accept="images/*"
+          onChange={onFileChange}
+        />
+        <label htmlFor="file-upload" className="filepicker-label">
+          Upload File
+        </label>
+      </div>
+    </div>
+  )
 }
 
 export default FilePicker
