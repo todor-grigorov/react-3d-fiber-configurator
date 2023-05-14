b3723d426a42a4b80a55e1f08a51ab169213157c
todor-grigorov <t.grigorov99@gmail.com>
2023-05-14 12:13:24 +0200

    feat: add outline variant of custom button

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -2,30 +2,48 @@ import React, { MouseEventHandler } from 'react'
 
 import state from '../store'
 import { useSnapshot } from 'valtio'
+import { color } from 'framer-motion'
 
 interface ParentProps {
-    type: string | 'filled'
-    title: string
-    customStyles?: string
-    handleClick: (e: any) => void
+  type: string | 'filled'
+  title: string
+  customStyles?: string
+  handleClick: (e: any) => void
 }
 
 type Props = ParentProps
 
-const CustomButton: React.FC<Props> = ({type, title, customStyles, handleClick}: Props): JSX.Element => {
-    const snap = useSnapshot(state)
+const CustomButton: React.FC<Props> = ({
+  type,
+  title,
+  customStyles,
+  handleClick,
+}: Props): JSX.Element => {
+  const snap = useSnapshot(state)
 
-    function generateStyle(type: string): React.CSSProperties | undefined {
-        if (type === 'filled') {
-            return {
-                backgroundColor: snap.color,
-                color: '#fff'
-            }
-        }
+  function generateStyle(type: string): React.CSSProperties | undefined {
+    if (type === 'filled') {
+      return {
+        backgroundColor: snap.color,
+        color: '#fff',
+      }
+    } else if (type === 'outline') {
+      return {
+        borderWidth: '1px',
+        borderColor: snap.color,
+        color: snap.color,
+      }
     }
+  }
 
   return (
-        <button className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`} style={generateStyle(type)} onClick={handleClick}>{title}</button>
+    <button
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      style={generateStyle(type)}
+      onClick={handleClick}
+    >
+      {title}
+    </button>
   )
 }
 
