d10dddbc689a05f9997591c03cf640eb0404494f
todor-grigorov <t.grigorov99@gmail.com>
2023-05-17 23:44:33 +0200

    feat: add props to AIPicker component

--- src/components/AIPicker.tsx
+++ src/components/AIPicker.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-const AIPicker: React.FC = (): JSX.Element => {
-  return (
-    <div>AIPicker</div>
-  )
+interface ParentProps {
+  prompt: string
+  setPrompt: React.Dispatch<React.SetStateAction<string>>
+  generatingImg: boolean
+}
+
+type Props = ParentProps
+
+const AIPicker: React.FC<Props> = ({
+  prompt,
+  setPrompt,
+  generatingImg,
+}: Props): JSX.Element => {
+  return <div>AIPicker</div>
 }
 
 export default AIPicker
